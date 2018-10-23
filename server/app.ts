b9e54777d28cb62ff7ab7e5eb00cef79a61b15c6
Efrain Vásquez <basselectricefra@gmail.com>
2018-10-23 12:29:11 -0500

    fav

--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,6 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
 import * as path from 'path';
-
 import setRoutes from './routes';
 
 const app = express();
