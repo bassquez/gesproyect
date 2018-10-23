24f72fd8d22d4ba4a7348365c94ca55bb1d9afbe
Efrain Vásquez <basselectricefra@gmail.com>
2018-10-23 13:38:58 -0500

    fav

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,7 @@ import setRoutes from './routes';
 
 const app = express();
 dotenv.load({ path: '.env' });
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(express.json());
